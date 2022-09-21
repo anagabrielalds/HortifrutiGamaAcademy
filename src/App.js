f9e2808be44f3a54e420d1c3ea8dc3ff7f2cedcb
Ana Gabriela Lima dos Santos <anagabrielalima91@gmail.com>
2022-09-21 13:30:30 -0300

    Teste hotjar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
+    
     <Router>
       <Switch>
           <Route exact path="/carrinho">
@@ -24,3 +25,12 @@ function App() {
 }
 
 export default App;
+
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:3168045,hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
